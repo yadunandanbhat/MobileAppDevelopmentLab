addeeeab7bf70649478df87115dff8045160e248
Yadunandan Bhat <yadunandanbhat@gmail.com>
2021-06-15 22:46:29 +0530

    Working SignUp activity

---
+++ SignUpandSignIn/app/src/main/java/com/example/signupandsignin/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.signupandsignin;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MainActivity extends AppCompatActivity {
+    EditText username, password;
+    Button signup;
+
+    String regex="^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@$!])[A-Za-z\\d@$!]{8,}$";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        signup = findViewById(R.id.signup);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userName = username.getText().toString();
+                String passWord = password.getText().toString();
+
+                if (validatePassword(passWord)) {
+                    Intent intent =  new Intent(MainActivity.this, MainActivity2.class);
+                    startActivity(intent);
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "Invlaid password", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    public boolean validatePassword(String pwd){
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(pwd);
+
+        return matcher.matches();
+
+
+    }
+}
